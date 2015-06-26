b178cfd7ebde277c7693539b6505483cd229a57f
“Jack <yellowponys@gmail.com>
2015-06-27 09:54:28 +1000

    Added hostnames under players and changed to % positions

--- cursor_game/views.py
+++ cursor_game/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
 def index(request):
-    request.session['ip'] = request.META.get('HTTP_HOST', request.META.get('REMOTE_HOST', request.META.get('HTTP_X_FORWARDED_FOR', request.META.get('HTTP_X_REAL_IP', request.META.get('REMOTE_ADDR', 'hacker')))))
+    request.session['id'] = request.META.get('HTTP_HOST', request.META.get('REMOTE_HOST', request.META.get('HTTP_X_FORWARDED_FOR', request.META.get('HTTP_X_REAL_IP', request.META.get('REMOTE_ADDR', 'hacker')))))
     return render(request, 'the.html')
