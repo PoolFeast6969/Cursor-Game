06326fdc444b01567dc79dae7e2c81f9da777229
poopfeast6969 <yellowponys@gmail.com>
2015-07-10 17:27:43 +1000

    Finally finshed the basic game. Now to add mechanics

--- cursor_game/connection.py
+++ cursor_game/connection.py
@@ -1,6 +1,6 @@
 from omnibus.factories import websocket_connection_factory
 
-game = []
+game = {}
 
 def gamelog(self, message):
     message = 'GAME: {0}'.format(message)
@@ -10,22 +10,24 @@ def gamelog(self, message):
 def player_connection(auth_class, pubsub):
     class GeneratedConnection(websocket_connection_factory(auth_class, pubsub)):
         def __init__(self, *args, **kwargs):
-            self.player = {'id':None,'username':None}
+            self.player = {'username':None,'picture':None}
+            self.id = None
             super(GeneratedConnection, self).__init__(*args, **kwargs)
 
         def command_subscribe(self, args):
             result = super(GeneratedConnection, self).command_subscribe(args)
             if result is not False:
-                self.player['id'] = self.authenticator.get_identifier()
                 self.player['username'] = self.request.host
-                game.append(self.player.copy())
+                self.player['picture'] = 'dunno'
+                self.id = self.authenticator.get_identifier()
+                game[self.id] = self.player.copy()
                 gamelog(self, 'added ' + self.player['username'] + ' to the game')
-                self.pubsub.publish('mousemoves', 'update', dict(enumerate(game)), sender='server')
+                self.pubsub.publish('mousemoves', 'update', game, sender='server')
 
         def close_connection(self):
-            game.remove(self.player)
+            game.__delitem__(self.id)
             gamelog(self, 'removed ' + self.player['username'] + ' from game')
-            self.pubsub.publish('mousemoves', 'update', dict(enumerate(game)), sender='server')
+            self.pubsub.publish('mousemoves', 'update', game, sender='server')
             return super(GeneratedConnection, self).close_connection()
 
     return GeneratedConnection
