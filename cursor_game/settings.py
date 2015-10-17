80d63235549430d97f8d79bd255a898b775d85fe
“Jack <yellowponys@gmail.com>
2015-10-17 20:25:44 +1100

    starts omnibus auto

--- cursor_game/settings.py
+++ cursor_game/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'omnibus',
+    'cursor_game',
 )
 
 MIDDLEWARE_CLASSES = (
--- omnibus/__init__.py
+++ omnibus/__init__.py
@@ -1,3 +1,3 @@
 import pkg_resources
-
+default_app_config = 'omnibus.apps.omnibusAppConfig'
 VERSION = pkg_resources.get_distribution('django_omnibus').version
